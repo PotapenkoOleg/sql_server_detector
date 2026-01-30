dbe0f45d44b223f6bdc1ad3edac8379189270ce8
Oleg Potapenko <oleg@example.com>
2026-01-29 23:23:47 -0500

    Default parameters fix

--- src/clap_parser/mod.rs
+++ src/clap_parser/mod.rs
@@ -3,12 +3,12 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 pub struct Args {
-    #[arg(long, short, default_value = "./FINAL/")]
+    #[arg(long, short, /*default_value = "./FINAL/"*/)]
     pub input_directory: String,
 
-    #[arg(long, short, default_value = "keywords.csv")]
+    #[arg(long, short)]
     pub keywords_file_name: Option<String>,
 
-    #[arg(long, short, default_value = "false")]
+    #[arg(long, short, default_value = "true")]
     pub load_sql_server_keywords: bool,
 }
